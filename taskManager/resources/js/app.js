c738d8fd29d869c3fc596354d97a9d8f6eb2f103
yuya haraguchi <your_email@example.com>
2024-02-12 20:55:58 +0900

    02-12

---
+++ taskManager/resources/js/app.js
@@ -0,0 +1,8 @@
+// resources/js/app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleComponent from './components/ExampleComponent';
+
+if (document.getElementById('example')) {
+  ReactDOM.render(<ExampleComponent />, document.getElementById('example'));
+}
---
+++ taskManager/resources/js/components/ExampleComponent.js
@@ -0,0 +1,8 @@
+// resources/js/components/ExampleComponent.js
+import React from 'react';
+
+const ExampleComponent = () => {
+  return <div>Hello, React!</div>;
+};
+
+export default ExampleComponent;
